94ef1bd4b7d0a7e4b90dd45e2a5d660208d9b79f
Devpe007 <anddev007pe@gmail.com>
2022-05-12 23:37:01 -0300

    create the Navbar component structure

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { FaBars } from 'react-icons/fa';
+
+import logo from '../../images/logo.png';
+
+import {
+  Nav,
+  NavContainer,
+  NavLogo,
+  NavImg,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from './NavbarElements';
+
+function Navbar() {
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo>
+          <NavImg src={logo} alt="logo" />
+        </NavLogo>
+
+        <MobileIcon>
+          <FaBars />
+        </MobileIcon>
+
+        <NavMenu>
+          <NavItem>
+            <NavLinks
+              to="home"
+            >
+              Home
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks
+              to="contacts"
+            >
+              Contatos
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks
+              to="meeting"
+            >
+              Agendar Reunião
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks
+              to="blog"
+            >
+              Blog Venus
+            </NavLinks>
+          </NavItem>
+        </NavMenu>
+      </NavContainer>
+    </Nav>
+  );
+}
+
+export default Navbar;
