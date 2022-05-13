93cd76553ecf14877bd8a0c1413cb5edf8bf5203
Devpe007 <anddev007pe@gmail.com>
2022-05-12 23:34:58 -0300

    add Roboto font-family in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,18 @@ module.exports = {
     'react',
   ],
   rules: {
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'no-extra-semi': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    'react/jsx-no-bind': 'off',
+    'react/prop-types': 'off',
+    'global-require': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-no-constructed-context-values': 'off',
+    'no-undef': 'off',
+    'react/jsx-filename-extension': 'off',
   },
 };
