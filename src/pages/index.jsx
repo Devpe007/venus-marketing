fa12d0e315263c6d513f0f21ed3bcb2afda3228a
Devpe007 <anddev007pe@gmail.com>
2022-05-13 08:38:06 -0300

    create the Home page

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+
+import Navbar from '../components/Navbar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <HeroSection />
+    </>
+  );
+};
+
+export default Home;
