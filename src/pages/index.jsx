b915f9f6e6893fcbcc7bcd1842f3e4468a2144ed
Devpe007 <anddev007pe@gmail.com>
2022-05-13 23:23:06 -0300

    add AboutUs component in Home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 
 import HeroSection from '../components/HeroSection';
 import ResultsSection from '../components/ResultsSection';
+import AboutUs from '../components/AboutUs';
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
 
       <HeroSection />
       <ResultsSection />
+      <AboutUs />
     </>
   );
 };
