806cbd0b0777107f94fc0a6429ef07972bdf3f8b
Devpe007 <anddev007pe@gmail.com>
2022-05-13 23:22:26 -0300

    styling AboutUs component

---
+++ src/components/AboutUs/AboutUsElements.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const AboutUsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #3a3a3a;
+`;
+
+export const AboutUsSection = styled.section`
+  margin-top: 50px;
+  margin-bottom: 30px;
+`;
+
+export const AboutUsContent = styled.div`
+  text-align: center;
+
+  padding: 0 100px 0 100px;
+`;
+
+export const AboutUsH1 = styled.h1`
+  font-size: 40px;
+
+  color: #fff;
+`;
+
+export const AboutUsP = styled.p`
+  font-size: 18px;
+
+  color: #787878;
+`;
