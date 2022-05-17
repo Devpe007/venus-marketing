140104b44327be2bd63f6137c92125c38ff145e8
Devpe007 <anddev007pe@gmail.com>
2022-05-16 22:07:55 -0300

    styling ValuesCompany component

---
+++ src/components/ValuesCompany/ValuesCompanyElements.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const ValuesCompanyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 100px;
+`;
+
+export const ValuesCompanyContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ValuesCompanyItems = styled.div`
+  padding: 0 30px 0 30px;
+
+  max-width: 400px;
+
+  text-align: center;
+`;
+
+export const ValuesCompanyImg = styled.div``;
+
+export const Img = styled.img`
+  width: 20%;
+
+  border-radius: 50px;
+`;
+
+export const ValuesCompanyH1 = styled.h1`
+  color: #5800CC;
+`;
+
+export const ValuesCompanyP = styled.p``;
